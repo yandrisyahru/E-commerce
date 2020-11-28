c68497b1445ee79676cb389c985ff14760956739
Yandri Syahru Ramayahya <yandrisyahru95@gmail.com>
2020-11-28 17:08:25 +0700

    -Cart page Dinamis

--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
 
 def store(request):
     products = Product.objects.all()
@@ -39,3 +40,6 @@ def checkout(request):
         'order':order,
     }
     return render (request, 'store/checkout.html', context)
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)
