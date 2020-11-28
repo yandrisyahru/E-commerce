e1026ba479f4a4d8103f1e06d2882a5f031ae307
Yandri Syahru Ramayahya <yandrisyahru95@gmail.com>
2020-11-28 15:49:36 +0700

    -Cart page Dinamis

--- Ecommerce/store/models.py
+++ Ecommerce/store/models.py
@@ -13,9 +13,17 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
